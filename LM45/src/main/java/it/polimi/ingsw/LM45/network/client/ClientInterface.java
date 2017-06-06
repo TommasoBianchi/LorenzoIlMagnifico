8fba89e1be83d00b43719b18789c1666da32d6cb
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-06-06 17:51:40 +0200

    Reimplemented lost methods in ClientInterface (TODO: rewrite code around it)

--- LM45/src/main/java/it/polimi/ingsw/LM45/network/client/ClientInterface.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/network/client/ClientInterface.java
@@ -1,6 +1,11 @@
 package it.polimi.ingsw.LM45.network.client;
 
+import java.io.IOException;
+
 public interface ClientInterface {
+	
+	public void setUsername(String username) throws IOException;
+	public void notifyPlayerTurn(String player) throws IOException;
 
 	/*// FIXME: think of the right methods to have here
 	public void updatePersonalBoard();
