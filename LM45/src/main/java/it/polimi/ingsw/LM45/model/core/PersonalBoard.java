1cbbe84c0e0b8a25c16f23e4180d46aeca4b3e87
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-06-10 18:21:53 +0200

    Javadoc on PersonalBoard

--- LM45/src/main/java/it/polimi/ingsw/LM45/model/core/PersonalBoard.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/model/core/PersonalBoard.java
@@ -19,8 +19,11 @@ public class PersonalBoard {
 	private Map<ResourceType, Integer> resources;
 	private List<CardEffect> permanentEffects;
 	private Resource[] territoryRequisites;
-	private List<Familiar> familiars; // Why?
 
+	/**
+	 * Initializes a new Board by instantiating the collections needed to hold resources, cards and permanentEffects.
+	 * In addition initializes the requisites for acquiring territory cards
+	 */
 	public PersonalBoard() {
 		this.cards = new EnumMap<CardType, List<Card>>(CardType.class);
 		this.resources = new EnumMap<ResourceType, Integer>(ResourceType.class);
@@ -31,11 +34,17 @@ public class PersonalBoard {
 				new Resource(ResourceType.MILITARY, 18) };
 	}
 
+	/**
+	 * @param resource the resource to add (it has to contain a positive amount)
+	 */
 	public void addResources(Resource resource) {
 		int newAmount = resources.getOrDefault(resource.getResourceType(), 0) + resource.getAmount();
 		resources.put(resource.getResourceType(), newAmount);
 	}
 
+	/**
+	 * @param resource the resource to remove (it has to contain a negative amount)
+	 */
 	public void removeResources(Resource resource) {
 		int newAmount = resources.getOrDefault(resource.getResourceType(), 0) - resource.getAmount();
 		if (newAmount < 0)
@@ -43,14 +52,27 @@ public class PersonalBoard {
 		resources.put(resource.getResourceType(), newAmount);
 	}
 
+	/**
+	 * @param resourceType the type of resource to count
+	 * @return the amount of resources of the given ResourceType
+	 */
 	public int getResourceAmount(ResourceType resourceType) {
 		return resources.getOrDefault(resourceType, 0);
 	}
 
+	/**
+	 * @param resource the type of resource to check
+	 * @return true if the amount of resource of the same type of the parameter resource
+	 * is at least the same as the amount of the parameter resource
+	 */
 	public boolean hasResources(Resource resource) {
 		return resources.getOrDefault(resource.getResourceType(), 0) >= resource.getAmount();
 	}
 
+	/**
+	 * @param card the card to check
+	 * @return true if we can add that card to this personalBoard (including check about territoryRequisites)
+	 */
 	public boolean canAddCard(Card card) {
 		boolean canAdd = getResourceAmount(card.getCardType().toResourceType()) < MAX_CARDS;
 		if (card.getCardType() == CardType.TERRITORY)
@@ -58,6 +80,9 @@ public class PersonalBoard {
 		return canAdd;
 	}
 
+	/**
+	 * @param card the card we want to add to this personalBoard
+	 */
 	public void addCard(Card card) {
 		if (cards.get(card.getCardType()) == null)
 			cards.put(card.getCardType(), new ArrayList<Card>());
@@ -66,20 +91,34 @@ public class PersonalBoard {
 				resources.getOrDefault(card.getCardType().toResourceType(), 0) + 1);
 	}
 
+	/**
+	 * Clears the requisites for the territory cards by setting them all to 0 military points
+	 */
 	public void clearTerritoryRequisites() {
 		territoryRequisites = Arrays.stream(territoryRequisites).map(resource -> new Resource(ResourceType.MILITARY, 0)).toArray(Resource[]::new);
 	}
 
+	/**
+	 * @param permanentEffect the CardEffect to add as a permanent effects
+	 */
 	public void addPermanentEffect(CardEffect permanentEffect) {
 		permanentEffects.add(permanentEffect);
 	}
 	
+	/**
+	 * @param effectResolutor the effectResolutor we need in order to resolve the harvest effects
+	 * @param value the value of the harvest action
+	 */
 	public void harvest(EffectResolutor effectResolutor, int value){
 		for(Card card : cards.get(CardType.TERRITORY)){
 			card.resolveEffect(effectResolutor, value);
 		}
 	}
-	
+
+	/**
+	 * @param effectResolutor the effectResolutor we need in order to resolve the production effects
+	 * @param value the value of the production action
+	 */
 	public void produce(EffectResolutor effectResolutor, int value){
 		for(Card card : cards.get(CardType.BUILDING)){
 			card.resolveEffect(effectResolutor, value);
