e109e23e0f3298719e5a155588dcc4a9513168be
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-06-10 18:25:02 +0200

    Javadoc on PersonalBonusTile

--- LM45/src/main/java/it/polimi/ingsw/LM45/model/core/PersonalBonusTile.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/model/core/PersonalBonusTile.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.LM45.model.core;
 
+import java.util.Arrays;
+
+import it.polimi.ingsw.LM45.model.effects.EffectResolutor;
+
 public class PersonalBonusTile {
 	
 	private static final int MIN_DICE = 1;
@@ -7,22 +11,30 @@ public class PersonalBonusTile {
 	private Resource[] productionBonuses;
 	private Resource[] harvestBonuses;
 	
+	/**
+	 * @param productionBonuses the base bonuses for production actions
+	 * @param harvestBonuses the base bonuses for harvest actions
+	 */
 	public PersonalBonusTile(Resource[] productionBonuses, Resource[] harvestBonuses){
 		this.productionBonuses = productionBonuses;
 		this.harvestBonuses = harvestBonuses;
+	}	
+	
+	/**
+	 * @param effectResolutor the effectResolutor we need in order to resolve the production effects
+	 * @param diceAmount the value of the production action
+	 */
+	public void harvest (EffectResolutor effectResolutor, int diceAmount){
+		if(diceAmount >= MIN_DICE)
+			Arrays.stream(harvestBonuses).forEach(resource -> effectResolutor.addResources(resource));
 	}
 	
-	public Resource[] produce (int diceAmount){
+	/**
+	 * @param effectResolutor the effectResolutor we need in order to resolve the production effects
+	 * @param diceAmount the value of the production action
+	 */
+	public void produce (EffectResolutor effectResolutor, int diceAmount){
 		if(diceAmount >= MIN_DICE)
-			return productionBonuses;
-		else
-			return new Resource[]{};
-	}	
-
-	public Resource[] harvest (int diceAmount){
-		if(diceAmount >= MIN_DICE)
-			return harvestBonuses;
-		else
-			return new Resource[]{};
+			Arrays.stream(productionBonuses).forEach(resource -> effectResolutor.addResources(resource));
 	}
 }
