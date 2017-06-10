0d359a2e0dc4b08ace7bcec9e2020f86d233f2e5
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-06-10 18:03:53 +0200

    Added javadoc to Familiar

--- LM45/src/main/java/it/polimi/ingsw/LM45/model/core/Familiar.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/model/core/Familiar.java
@@ -11,6 +11,10 @@ public class Familiar {
 	private int servantBonusCost;
 	private boolean isPlaced;
 	
+	/**
+	 * @param player the player owning this familiar
+	 * @param familiarColor the color of this familiar (WHITE, ORANGE, BLACK or UNCOLORED)
+	 */
 	public Familiar(Player player, FamiliarColor familiarColor){
 		this.player = player;
 		this.familiarColor = familiarColor;
@@ -22,14 +26,24 @@ public class Familiar {
 		this.isPlaced = false;
 	}
 	
+	/**
+	 * clears any bonus given to this familiar by spending servants
+	 */
 	public void clearServantsBonus(){
 		servantBonus = 0;
 	}
 	
+	/**
+	 * @return the value of this familiar (including bonuses)
+	 */
 	public int getValue(){
 		return value + servantBonus + otherBonuses;
 	}
 	
+	/**
+	 * @param value the new value of this familiar
+	 * @param valueIsStatic set this to true to lock the value of this familiar between different turns
+	 */
 	public void setValue(int value, boolean valueIsStatic){
 		if(!valueIsStatic){
 			this.value = value;
@@ -37,35 +51,59 @@ public class Familiar {
 		}
 	}
 	
+	/**
+	 * @param value the new value of this familiar
+	 */
 	public void setValue(int value){
 		setValue(value, false);
 	}
 	
+	/**
+	 * @param bonus a new bonus to add to this familiar (not given by servants)
+	 */
 	public void addBonus(int bonus){
 		this.otherBonuses += bonus;
 	}
 	
+	/**
+	 * @return the player owning this familiar
+	 */
 	public Player getPlayer(){
 		return this.player;
 	}
 	
+	/**
+	 * @return the color of this familiar (WHITE, ORANGE, BLACK or UNCOLORED)
+	 */
 	public FamiliarColor getFamiliarColor(){
 		return this.familiarColor;
 	}
 	
+	/**
+	 * Spend a servant (or more if servantBonusCost has been modified) to increase by one the value of this familiar
+	 */
 	public void addServantsBonus(){
 		servantBonus++;
 		this.player.addResources(new Resource(ResourceType.SERVANTS, -servantBonusCost));
 	}
 	
+	/**
+	 * @return whether or not this familiar has been placed in a slot
+	 */
 	public boolean getIsPlaced(){
 		return this.isPlaced;
 	}
 	
+	/**
+	 * @param value whether or not this familiar has been placed in a slot
+	 */
 	public void setIsPlaced(boolean value){
 		this.isPlaced = value;
 	}
 	
+	/**
+	 * @param cost the cost (in servants) of increasing by one the value of this familiar 
+	 */
 	public void setServantBonusCost(int cost){
 		this.servantBonusCost = cost;
 	}
