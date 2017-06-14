5aa9f04240f852c9341ee18e51b41dd5d5ecb57a
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-06-14 17:17:58 +0200

    Implemented PersonalBonusTile.toString

--- LM45/src/main/java/it/polimi/ingsw/LM45/model/core/PersonalBonusTile.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/model/core/PersonalBonusTile.java
@@ -5,36 +5,49 @@ import java.util.Arrays;
 import it.polimi.ingsw.LM45.model.effects.EffectResolutor;
 
 public class PersonalBonusTile {
-	
+
 	private static final int MIN_DICE = 1;
 
 	private Resource[] productionBonuses;
 	private Resource[] harvestBonuses;
-	
+
 	/**
-	 * @param productionBonuses the base bonuses for production actions
-	 * @param harvestBonuses the base bonuses for harvest actions
+	 * @param productionBonuses
+	 *            the base bonuses for production actions
+	 * @param harvestBonuses
+	 *            the base bonuses for harvest actions
 	 */
-	public PersonalBonusTile(Resource[] productionBonuses, Resource[] harvestBonuses){
+	public PersonalBonusTile(Resource[] productionBonuses, Resource[] harvestBonuses) {
 		this.productionBonuses = productionBonuses;
 		this.harvestBonuses = harvestBonuses;
-	}	
-	
+	}
+
 	/**
-	 * @param effectResolutor the effectResolutor we need in order to resolve the production effects
-	 * @param diceAmount the value of the production action
+	 * @param effectResolutor
+	 *            the effectResolutor we need in order to resolve the production effects
+	 * @param diceAmount
+	 *            the value of the production action
 	 */
-	public void harvest (EffectResolutor effectResolutor, int diceAmount){
-		if(diceAmount >= MIN_DICE)
+	public void harvest(EffectResolutor effectResolutor, int diceAmount) {
+		if (diceAmount >= MIN_DICE)
 			Arrays.stream(harvestBonuses).forEach(resource -> effectResolutor.addResources(resource));
 	}
-	
+
 	/**
-	 * @param effectResolutor the effectResolutor we need in order to resolve the production effects
-	 * @param diceAmount the value of the production action
+	 * @param effectResolutor
+	 *            the effectResolutor we need in order to resolve the production effects
+	 * @param diceAmount
+	 *            the value of the production action
 	 */
-	public void produce (EffectResolutor effectResolutor, int diceAmount){
-		if(diceAmount >= MIN_DICE)
+	public void produce(EffectResolutor effectResolutor, int diceAmount) {
+		if (diceAmount >= MIN_DICE)
 			Arrays.stream(productionBonuses).forEach(resource -> effectResolutor.addResources(resource));
+	}
+
+	@Override
+	public String toString() {
+		return "Personal Bonuses\nProduction: "
+				+ Arrays.stream(productionBonuses).map(resource -> resource.toString()).reduce("", (a, b) -> a + " " + b) + "\nHarvest: "
+				+ Arrays.stream(harvestBonuses).map(resource -> resource.toString()).reduce("", (a, b) -> a + " " + b);
 	}
 }
