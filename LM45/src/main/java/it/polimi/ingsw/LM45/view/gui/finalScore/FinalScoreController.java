92add4152598ea8024ba7eca794829e244d2e39c
Kostandin Caushi <kostandin.caushi@mail.polimi.it>
2017-06-24 00:00:45 +0200

    Added finalScore view

---
+++ LM45/src/main/java/it/polimi/ingsw/LM45/view/gui/finalScore/FinalScoreController.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.LM45.view.gui.finalScore;
+
+import it.polimi.ingsw.LM45.model.core.PlayerColor;
+
+public class FinalScoreController {
+	
+	
+	
+	public FinalScoreController(String[] playersUsername, PlayerColor[] playerColors, int[] scores){
+		
+	}
+}
