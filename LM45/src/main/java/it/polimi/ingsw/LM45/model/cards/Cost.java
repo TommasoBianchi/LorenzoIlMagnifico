a890f09f451d1e4e1dcc51b2706035a3c04e9bf3
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-06-23 21:17:31 +0200

    Fixed a ConcurrentModificationException in cost
    In getResourcesToPay the final stream was both picking element from a
    map and removing elements from the same map

--- LM45/src/main/java/it/polimi/ingsw/LM45/model/cards/Cost.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/model/cards/Cost.java
@@ -2,7 +2,9 @@ package it.polimi.ingsw.LM45.model.cards;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import it.polimi.ingsw.LM45.model.core.Player;
@@ -35,8 +37,7 @@ public class Cost implements Serializable {
 	 * @return true if the player can pay the cost modified (either increased or decreased) by the action modifier
 	 */
 	public boolean canPay(EffectResolutor effectResolutor, ActionModifier actionModifier) {
-		return getResourcesToPay(actionModifier).filter(resource -> resource.getAmount() > 0)
-				.allMatch(resource -> effectResolutor.hasResources(resource));
+		return getResourcesToPay(actionModifier).allMatch(resource -> effectResolutor.hasResources(resource));
 	}
 
 	/**
@@ -46,7 +47,8 @@ public class Cost implements Serializable {
 	 *            the actionModifier for the action the player is trying to do
 	 */
 	public void pay(EffectResolutor effectResolutor, ActionModifier actionModifier) {
-		getResourcesToPay(actionModifier).filter(resource -> resource.getAmount() > 0).forEach(resource -> effectResolutor.addResources(resource));
+		getResourcesToPay(actionModifier).map(resource -> resource.getAmount() > 0 ? resource.multiply(-1) : resource) // Make sure to subtract resources
+		 .forEach(resource -> effectResolutor.addResources(resource));
 	}
 
 	/**
@@ -58,17 +60,19 @@ public class Cost implements Serializable {
 
 	private Stream<Resource> getResourcesToPay(ActionModifier actionModifier) {
 		Map<ResourceType, Integer> costModifiers = actionModifier.getCostModifiers();
+		Set<ResourceType> incrementedResources = new HashSet<>();
 		Stream<Resource> resourcesToPay = Arrays.stream(costResources).map(resource -> {
 			Resource incrementedResource = resource.increment(costModifiers.getOrDefault(resource.getResourceType(), 0));
-			costModifiers.remove(resource.getResourceType());
+			System.out.println(incrementedResource);
+			incrementedResources.add(resource.getResourceType());
 			return incrementedResource;
-		}).map(resource -> resource.getAmount() > 0 ? resource.multiply(-1) : resource); // To make sure to subtract resources
+		});
 
 		// Make sure you pay also resources in the costModifier that were not present in the original cost
 		// (i.e. if this was an empty cost and the costModifier contains +3 COINS, than you have to pay them)
-		resourcesToPay = Stream.concat(resourcesToPay,
-				costModifiers.entrySet().stream().map(entry -> new Resource(entry.getKey(), -entry.getValue())));
-		return resourcesToPay;
+		resourcesToPay = Stream.concat(resourcesToPay, costModifiers.entrySet().stream()
+				.filter(entry -> !incrementedResources.contains(entry.getKey())).map(entry -> new Resource(entry.getKey(), entry.getValue())));
+		return resourcesToPay.filter(resource -> resource.getAmount() > 0);
 	}
 
 	@Override
