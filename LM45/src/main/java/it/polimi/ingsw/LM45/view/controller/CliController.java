01bcd3e8c3f57f1b255bb85692c1ac9e8ec2f362
Kostandin Caushi <kostandin.caushi@mail.polimi.it>
2017-07-05 13:56:43 +0200

    Started implementing CliController methods

--- LM45/src/main/java/it/polimi/ingsw/LM45/view/controller/CliController.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/view/controller/CliController.java
@@ -1,7 +1,143 @@
 package it.polimi.ingsw.LM45.view.controller;
 
-public class CliController {
+import it.polimi.ingsw.LM45.model.cards.Card;
+import it.polimi.ingsw.LM45.model.cards.Excommunication;
+import it.polimi.ingsw.LM45.model.cards.LeaderCard;
+import it.polimi.ingsw.LM45.model.cards.PeriodType;
+import it.polimi.ingsw.LM45.model.core.FamiliarColor;
+import it.polimi.ingsw.LM45.model.core.PersonalBonusTile;
+import it.polimi.ingsw.LM45.model.core.PlayerColor;
+import it.polimi.ingsw.LM45.model.core.Resource;
+import it.polimi.ingsw.LM45.model.core.SlotType;
+import it.polimi.ingsw.LM45.network.client.ClientController;
 
-	// TODO: implement
-	
+public class CliController implements ViewInterface {
+
+	@Override
+	public void showLeaderCardChoiceView() {
+		System.out.println("Welcome to Lorenzo il Magnifico !\n");
+		System.out.println("--> LeaderCard Choice Phase\n");
+	}
+
+	@Override
+	public void initializeGameBoard(String[] playersUsername, PlayerColor[] playerColors,
+			Excommunication[] excommunications) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addFamiliar(SlotType slotType, int position, FamiliarColor familiarColor, PlayerColor playerColor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pickCard(Card card, String username) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addCardsOnTower(Card[] cards, SlotType slotType) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFamiliar(String username, FamiliarColor color, int value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notifyError(String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doBonusAction(SlotType slotType, int value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int chooseFrom(String[] alternatives) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setClientController(ClientController clientController) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setResources(Resource[] resources, String username) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void myTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playerTurn(String username) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLeaderCards(LeaderCard[] leaders) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void discardLeaderCard(String username, LeaderCard leader) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playLeaderCard(String username, LeaderCard leader) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void activateLeaderCard(String username, LeaderCard leader) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enableLeaderCard(String username, LeaderCard leader) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPersonalBonusTile(String username, PersonalBonusTile personalBonusTile) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void placeExcommunicationToken(PlayerColor playerColor, PeriodType periodType) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showFinalScore(String[] playersUsername, PlayerColor[] playerColors, int[] scores) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
