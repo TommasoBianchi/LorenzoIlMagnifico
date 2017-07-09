ce7a896b61f2739f2bbb4b079ba1eb1ac4e33ad7
Kostandin Caushi <kostandin.caushi@mail.polimi.it>
2017-07-10 00:32:01 +0200

    Inserted ConsoleWriter Javadoc

--- LM45/src/main/java/it/polimi/ingsw/LM45/view/cli/ConsoleWriter.java
+++ LM45/src/main/java/it/polimi/ingsw/LM45/view/cli/ConsoleWriter.java
@@ -5,6 +5,14 @@ import com.diogonunes.jcdp.color.api.Ansi.Attribute;
 import com.diogonunes.jcdp.color.api.Ansi.BColor;
 import com.diogonunes.jcdp.color.api.Ansi.FColor;
 
+/**
+ * Class this different methods to print with different colors.
+ * It has also inside an Enum "ConsoleColor" with all colors
+ * that can be used to print
+ * 
+ * @author Kostandin
+ *
+ */
 public class ConsoleWriter {
 	
 	public enum ConsoleColor {
@@ -21,15 +29,25 @@ public class ConsoleWriter {
 		private FColor fontColor;
 		private BColor backgroundColor;
 		
+		/**
+		 * @param fontColor the color of the font
+		 * @param backgroundColor the color of the background
+		 */
 		private ConsoleColor(FColor fontColor, BColor backgroundColor){
 			this.fontColor = fontColor;
 			this.backgroundColor = backgroundColor;
 		}
 		
+		/**
+		 * @return the fontColor from a ConsoleColor
+		 */
 		protected FColor toFontColor(){
 			return this.fontColor;
 		}
 		
+		/**
+		 * @return the backgroundColor from a ConsoleColor
+		 */
 		protected BColor toBackgroundColor() {
 			return this.backgroundColor;
 		}
@@ -37,35 +55,62 @@ public class ConsoleWriter {
 	
 	private static ColoredPrinter coloredPrinter = new ColoredPrinter.Builder(1, false).build();
 	
+	/**
+	 * @param message the message to print
+	 * @param color the consoleColor to use for fontColor
+	 * @param backgroundColor the consoleColor to use for backgroundColor
+	 */
 	public static void println(String message, ConsoleColor color, ConsoleColor backgroundColor){
 		coloredPrinter.println(message, Attribute.NONE, color.toFontColor(), backgroundColor.toBackgroundColor());
 		coloredPrinter.clear();
 	}
 	
+	/**
+	 * @param message the message to print
+	 * @param color the consoleColor to use for fontColor
+	 */
 	public static void println(String message, ConsoleColor color){
 		println(message, color, ConsoleColor.BLACK);
 	}
 	
+	/**
+	 * @param message the message to print
+	 */
 	public static void println(String message){
 		println(message, ConsoleColor.NONE);
 	}
 	
+	/**
+	 * @param message the message to print
+	 */
 	public static void printCommand(String message) {
 		println(message, ConsoleColor.CYAN);
 	}
 	
+	/**
+	 * @param message the message to print
+	 */
 	public static void printShowInfo(String message) {
 		println(message, ConsoleColor.YELLOW);
 	}
 	
+	/**
+	 * @param message the message to print
+	 */
 	public static void printError(String message) {
 		println(message, ConsoleColor.RED);
 	}
 	
+	/**
+	 * @param message the message to print
+	 */
 	public static void printValidInput(String message) {
 		println(message, ConsoleColor.GREEN);
 	}
 	
+	/**
+	 * @param message the message to print
+	 */
 	public static void printChoice(String message) {
 		println(message, ConsoleColor.WHITE);
 	}
